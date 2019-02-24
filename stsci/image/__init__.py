b528fe35b672fa90e5ea87513a5d878f744f190c
James Davies <jdavies@stsci.edu>
2019-02-23 23:57:56 -0500

    Remove stsci.tools.tester dependency

--- stsci/image/__init__.py
+++ stsci/image/__init__.py
@@ -4,10 +4,3 @@ from ._image import *
 from .combine import *
 
 from .version import *
-
-try:
-    import stsci.tools.tester
-    def test(*args,**kwds):
-        stsci.tools.tester.test(modname=__name__, *args, **kwds)
-except ImportError:
-    pass
