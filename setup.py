e837ba8bb9bb1f30ec957dc4ed6a5be340c44ef9
James Davies <jdavies@stsci.edu>
2021-08-30 14:02:57 -0400

    Add [test] extras to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from numpy import get_include as np_include
 from setuptools import setup, find_packages, Extension
-
+from numpy import get_include as np_include
 
 setup(
-    name = 'stsci.image',
-    author = 'STScI',
-    author_email = 'help@stsci.edu',
-    description = 'Image array manipulation functions',
-    url = 'https://github.com/spacetelescope/stsci.image',
-    classifiers = [
+    name='stsci.image',
+    author='STScI',
+    author_email='help@stsci.edu',
+    description='Image array manipulation functions',
+    url='https://github.com/spacetelescope/stsci.image',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -16,20 +15,25 @@ setup(
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires = [
+    install_requires=[
         'numpy>=1.13',
         'scipy',
     ],
-    packages = find_packages(),
-    package_data = {
+    packages=find_packages(),
+    package_data={
         '': ['LICENSE.txt'],
     },
     ext_modules=[
         Extension('stsci.image._combine',
-            ['src/_combinemodule.c'],
-            include_dirs=[np_include()],
-            define_macros=[('NUMPY','1')]),
+                  ['src/_combinemodule.c'],
+                  include_dirs=[np_include()],
+                  define_macros=[('NUMPY', '1')]),
     ],
-    use_scm_version = True,
-    setup_requires = ['setuptools_scm'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    extras_require={
+        "test": [
+            "pytest"
+        ]
+    }
 )
